7575b779eba57b25deeac83da500a3376cfb03a2
Dan Harrison <harrison92d@gmail.com>
2023-10-24 14:08:27 +0100

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,20 +1,10 @@
-$console = $Host.UI.RawUI
-$console.BackgroundColor = "black"
-Clear-Host
-Set-PSReadlineOption -BellStyle None
-
-if (-Not (Get-Module -ListAvailable -Name posh-git)) {
-    Install-Module posh-git -Scope CurrentUser -Force
+Function mainify
+{
+    $branches = git branch
+    foreach ($branch in $branches)
+    {
+        if ($branch -ne "* master") {
+            git branch -D $branch.Trim()
+        }
+    }
 }
-Import-Module posh-git
-function prompt {
-    $prompt += & $GitPromptScriptBlock
-    $prompt += Write-Prompt "`n"
-    "$prompt"
-}
-
-
-if (-Not (Get-Module -ListAvailable -Name DockerCompletion)) {
-    Install-Module DockerCompletion -Scope CurrentUser -Force
-}
-Import-Module DockerCompletion
