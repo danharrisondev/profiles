addef5d5cbf2d4897601120e906859c45e5ba5e6
Dan Harrison <harrison92d@gmail.com>
2023-10-25 08:53:51 +0100

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,9 +1,16 @@
 Function mainify
 {
+    $currentBranch = git branch --show-current
+
+    if ($currentBranch -notin "master", "main") {
+        Write-Host "Please switch to master/main branch before running mainify"
+        return
+    }
+
     foreach ($branch in git branch)
     {
-        if ($branch -notin '* master', '* main') {
-            git branch -D $branch.Trim()
+        if ($branch.Trim() -notin "* master", "* main", "master", "main") {
+            git branch -d $branch.Trim()
         }
     }
 }
